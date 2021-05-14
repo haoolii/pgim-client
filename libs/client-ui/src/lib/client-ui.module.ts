1623bc357fc742905ee3151eb282c0ef46cf49f6
Hao <unnhao@gmail.com>
2021-05-14 11:49:04 +0800

    feat: create basic lib

--- libs/client-ui/src/lib/client-ui.module.ts
+++ libs/client-ui/src/lib/client-ui.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from './button/button.module';
+import { ModalModule } from './modal/modal.module';
+import { RadioModule } from './radio/radio.module';
+import { CheckboxModule } from './checkbox/checkbox.module';
+import { IconModule } from './icon/icon.module';
+import { ProgressModule } from './progress/progress.module';
+import { PageHeaderModule } from './page-header/page-header.module';
+import { PageFooterModule } from './page-footer/page-footer.module';
+import { I18nModule } from './i18n/i18n.module';
 
 @NgModule({
-  imports: [CommonModule, ButtonModule],
+  imports: [CommonModule, ButtonModule, ModalModule, RadioModule, CheckboxModule, IconModule, ProgressModule, PageHeaderModule, PageFooterModule, I18nModule],
   declarations: [],
   exports: [ButtonModule]
 })
